0ad72e34cd322acf0c003772b6adf34579f4dfa2
Joël Henri <ebosqua.edimo@gmail.com>
2023-09-03 23:46:54 +0200

    Les points

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,15 @@ import { useState } from 'react'
 // const words1 = ["DOSER","BIBLE","SERUM","BANCS","PILON","CIBLE","PAGNE","SABLE","MAIRE","DOIGT"]
 // const words2 = ["HOPITAL","RADICAL","EXCUSER","MEDICAL","PRODUIT","PORTAIL","BROMURE","PRALINE","ABRICOT","DRIBBLE"]
 // const words3 = ["SUISSESSE","EMBARQUER","PLASTIQUE","ETUDIANTE","FLEMMARDE","ESCAPADES","VERRIERES","PESTICIDE","ACCOUDOIR","CHATELAIN"]
-let test = 1
+
 
 const words = ["SUISSESSE","EMBARQUER","PLASTIQUE","ETUDIANTE","FLEMMARDE","ESCAPADES","VERRIERES","PESTICIDE","ACCOUDOIR","CHATELAIN"]
 const chosenword = words[parseInt(Math.random()*10)]
 
 export default function Page(){
     const [letter,setLetter] = useState('')
+    const [word,setWord] = useState(["_","_","_","_","_","_","_","_","_","_"])
+    const [points, setPoints] = useState(0)
     // let b = "bonjour"
     // b[2] = "s"
     // const [chosenword2,setChosenWord] = useState(chosenword)
@@ -18,13 +20,13 @@ export default function Page(){
     for(let element of chosenword){
         chosenword2.push(element)
     }
-    const [word,setWord] = useState(["_","_","_","_","_","_","_","_","_","_"])
     function handleChangeLetter(lettre){
         let i = 0    
         setWord(chosenword2.map(element => {
                     i++
                     if (element === lettre){
-                       return element
+                       setPoints(p => p + 10)
+                       return lettre
                     }
                     else if(word[i-1] !== "_"){
                         return word[i-1]
@@ -45,10 +47,11 @@ export default function Page(){
     }
     return (
     <div>
+        <p>{points} points</p>
         <p>{word}</p>
         <p>{chosenword}</p>
         <form onChange={(e) => onChange2(e)}>
-         <input type="text" maxlength="1"></input>
+         <input type="text" maxLength="1"></input>
         </form>
         {/* <ChangeLetter lettre={letter} /> */}
         {/* <p>{b.length}</p> */}
